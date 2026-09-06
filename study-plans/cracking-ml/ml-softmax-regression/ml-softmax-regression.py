9140b83383f9c52b14084d9f68f7a3818ae0dc2d
sahil882003 <118373343+sahil882003@users.noreply.github.com>
2026-09-06 11:10:14 +0530

    Solve ml-softmax-regression

--- study-plans/cracking-ml/ml-softmax-regression/ml-softmax-regression.py
+++ study-plans/cracking-ml/ml-softmax-regression/ml-softmax-regression.py
@@ -10,9 +10,14 @@ def softmax_regression(X, y, n_classes, lr=0.01, n_iters=1000):
     Returns: tuple (weights, bias) where weights is a 2D list (d x K) and bias is a list of length K
     """
     X = np.array(X,dtype = float)
-    y = np.array(y,dtype = float)
+    y = np.array(y,dtype = int)
     n,d = X.shape
 
+    one_hot_encoded_y = np.zeros((n,n_classes))
+    one_hot_encoded_y[np.arange(0,n),y] = 1
+
+    y = one_hot_encoded_y
+    
     weights = np.zeros((d,n_classes))
     biases = np.zeros(n_classes)
 
@@ -24,13 +29,14 @@ def softmax_regression(X, y, n_classes, lr=0.01, n_iters=1000):
         raw_scores = raw_scores - np.max(raw_scores,axis = 1).reshape(-1,1)
         raw_scores = array_exp(raw_scores)
         class_probs = raw_scores / np.sum(raw_scores, axis = 1).reshape(-1,1)
-        for j in range(n_classes):
-            curr_cls_enc = (y == j).astype(float)
-            errors = class_probs[:,j] - curr_cls_enc
-            errors = errors.reshape(-1,1)
-            weights[:,j] = weights[:,j] - (lr * 1/n * (X.T @ (errors)).reshape(d))
-            biases[j] = biases[j] - (lr * 1/n * (np.sum(errors)))
+       
+        gradient_weights =1/n * X.T @ (class_probs - y)
+        gradient_biases = 1/n * np.sum(class_probs - y,axis = 0)
 
+        weights = weights - lr * gradient_weights
+        biases = biases - lr * gradient_biases
+
+        
 
     return weights,biases
         
